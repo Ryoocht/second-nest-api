8dc53d333754bcecfcbe16ed73ae14f3be7da740
Ryochi <ryo1.t.zubu@gmail.com>
2022-09-14 08:22:21 +1000

    adding docker and setup database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return 'I have signuped up';
+  }
+
+  signin() {
+    return 'I have signined in';
+  }
+}
